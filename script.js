35c9ea9c3e9c5ad7700d56da42acb882d3bbcd8e
Tomas <tomas.m.m.d.rodrigues@gmail.com>
2024-02-12 19:30:25 +0000

    Display and button interaction changed to display the operations in form of string instead of an array

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let number1;
-let operator;
-let number2;
-let displayContent = [];
-let index = 0;
-
 const displayScreen = document.querySelector("#display");
 const btnClear = document.querySelector("#clearBtn");
 const btnDelete = document.querySelector("#deleteBtn");
@@ -24,138 +18,105 @@ const btnSubtract = document.querySelector("#btnSubtract");
 const btnEqual = document.querySelector("#btnEqual");
 const btnPoint = document.querySelector("#btnPoint");
 
+let firstNumber = "";
+let secondNumber = "";
+let operator = null;
+let displayMem = "";
 
 
-/////////////////////////////////////BUTTONS/////////////////////////////////////
-
-btnClear.addEventListener("click", () => 
-{
-    displayContent.length = 0;
-    displayScreen.textContent = displayContent.toString();
-    index = 0;
+btnClear.addEventListener("click", () => {
+    clear();
 })
 
-btnDelete.addEventListener("click", () => 
-{
-    displayContent.pop();
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    if(index > 0)
-    {
-        index--;
-    }
+btnDelete.addEventListener("click", () => {
+    deleteLastCharacter();
 })
 
-btn0.addEventListener("click", () => 
-{
-    displayContent[index] = 0;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn0.addEventListener("click", () => {
+    append("0");
 })
 
-btn1.addEventListener("click", () => 
-{
-    displayContent[index] = 1;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn1.addEventListener("click", () => {
+    append("1");
 })
 
-btn2.addEventListener("click", () => 
-{
-    displayContent[index] = 2;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn2.addEventListener("click", () => {
+    append("2");
 })
 
-btn3.addEventListener("click", () => 
-{
-    displayContent[index] = 3;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn3.addEventListener("click", () => {
+    append("3");
 })
 
-btn4.addEventListener("click", () => 
-{
-    displayContent[index] = 4;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn4.addEventListener("click", () => {
+    append("4");
 })
 
-btn5.addEventListener("click", () => 
-{
-    displayContent[index] = 5;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn5.addEventListener("click", () => {
+    append("5");
 })
 
-btn6.addEventListener("click", () => 
-{
-    displayContent[index] = 6;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn6.addEventListener("click", () => {
+    append("6");
 })
 
-btn7.addEventListener("click", () => 
-{
-    displayContent[index] = 7;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn7.addEventListener("click", () => {
+    append("7");
 })
 
-btn8.addEventListener("click", () => 
-{
-    displayContent[index] = 8;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn8.addEventListener("click", () => {
+    append("8");
 })
 
-btn9.addEventListener("click", () => 
-{
-    displayContent[index] = 9;
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btn9.addEventListener("click", () => {
+    append("9");
 })
 
-btnTimes.addEventListener("click", () => 
-{
-    displayContent[index] = '×';
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btnPoint.addEventListener("click", () => {
+    append(".");
 })
 
-btnDivide.addEventListener("click", () => 
-{
-    displayContent[index] = '÷';
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btnAdd.addEventListener("click", () => {
+    append("+");
 })
 
-btnAdd.addEventListener("click", () => 
-{
-    displayContent[index] = '+';
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btnSubtract.addEventListener("click", () => {
+    append("-");
 })
 
-btnSubtract.addEventListener("click", () => 
-{
-    displayContent[index] = '-';
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btnTimes.addEventListener("click", () => {
+    append("×");
 })
 
-btnPoint.addEventListener("click", () => 
-{
-    displayContent[index] = '.';
-    displayScreen.textContent = displayContent.toString().replaceAll(",","");
-    index++;
+btnDivide.addEventListener("click", () => {
+    append("÷");
 })
 
-btnEqual.addEventListener("click", () => 
-{
-    
-})
 
-/////////////////////////////////////FUNCTIONS/////////////////////////////////////
+/////////////////////////////////////LOGIC FUNCTIONS/////////////////////////////////////////
+
+function append(character)
+{
+    displayMem += character;
+    displayScreen.textContent = displayMem;
+}
+
+function clear()
+{
+    displayMem = "";
+    displayScreen.textContent = displayMem;
+}
+
+function deleteLastCharacter()
+{
+    displayMem = displayMem.substring(0, displayMem.length - 1);
+    displayScreen.textContent = displayMem;
+}
+
+
+
+
+/////////////////////////////////////OPERATION FUNCTIONS/////////////////////////////////////
 
 function operate(number1, number2, operator)
 {
@@ -174,7 +135,6 @@ function operate(number1, number2, operator)
         return divide(number1,number2);
     }
 }
-
 
 function add(number1, number2)
 {
